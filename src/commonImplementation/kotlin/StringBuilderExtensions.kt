bb929acb388a86004f1b9e2095211b660669d2d2
Jake Wharton <jw@squareup.com>
2023-01-25 23:29:05 -0500

    Inline implementations in actuals
    With the dedicated platform testing gone, this indirection is no longer needed. The common tests still validate behavioral conformity across all platforms.

--- src/commonImplementation/kotlin/StringBuilderExtensions.kt
+++ src/commonImplementation/kotlin/StringBuilderExtensions.kt
@@ -1,8 +1,15 @@
 package de.cketti.codepoints
 
+import de.cketti.codepoints.CodePoints.highSurrogate
+import de.cketti.codepoints.CodePoints.isBmpCodePoint
+import de.cketti.codepoints.CodePoints.lowSurrogate
 import kotlin.text.StringBuilder
-import de.cketti.codepoints.internal.appendCodePoint as commonAppendCodePoint
 
 actual fun StringBuilder.appendCodePoint(codePoint: Int): StringBuilder = apply {
-    commonAppendCodePoint(this, codePoint)
+    if (isBmpCodePoint(codePoint)) {
+        append(codePoint.toChar())
+    } else {
+        append(highSurrogate(codePoint))
+        append(lowSurrogate(codePoint))
+    }
 }
