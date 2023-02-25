52a94c3b027cec690f43b405310410db102207c2
cketti <ck@cketti.de>
2023-02-25 15:42:20 +0100

    Rename `StringBuilderExtensions*` files to `AppendableExtensions*`

--- kotlin-codepoints-deluxe/src/commonTest/kotlin/AppendableExtensionsTest.kt
+++ kotlin-codepoints-deluxe/src/commonTest/kotlin/AppendableExtensionsTest.kt
@@ -1,18 +1,19 @@
-package de.cketti.codepoints
+package de.cketti.codepoints.deluxe
 
 import kotlin.test.assertEquals
 import kotlin.test.Test
 
-class StringBuilderExtensionsTest {
+class AppendableExtensionsTest {
     @Test
     fun appendCodePoint() {
         val actual = buildString {
-            appendCodePoint('a'.code)
+            appendCodePoint('a'.toCodePoint())
             append('b')
-            appendCodePoint("\uFFFF".codePointAt(0))
+            appendCodePoint('\uFFFF'.toCodePoint())
             append('c')
             appendCodePoint("\uD83E\uDD95".codePointAt(0))
         }
+        
         assertEquals("ab\uFFFFc\uD83E\uDD95", actual)
     }
 }
